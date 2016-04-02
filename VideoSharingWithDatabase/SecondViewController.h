7057cd286469a7bed0a0d1e8d00ad9f539bb0f82
Catherine Zhao <iecatherine@hotmail.ca>
2016-04-02 18:47:37 -0400

    Initial Commit

---
+++ VideoSharingWithDatabase/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  VideoSharingWithDatabase
+//
+//  Created by Catherine Zhao on 2016-04-02.
+//  Copyright © 2016 Catherine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ VideoSharingWithDatabase/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoSharingWithDatabase
+//
+//  Created by Catherine Zhao on 2016-04-02.
+//  Copyright © 2016 Catherine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
